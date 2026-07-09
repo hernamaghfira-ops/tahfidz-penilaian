09fedad8c652288192505a6237b8a4e085fcb10f
hernamaghfira-ops <hernamaghfira@gmail.com>
2026-07-09 09:00:36 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
+// ============================
+// TahfidzKu v1.0
+// ============================
 
+document.addEventListener("DOMContentLoaded", function () {
+
+    const dashboard = {
+        totalSantri: 0,
+        setoranHariIni: 0,
+        murojaahHariIni: 0,
+        rataRata: 0
+    };
+
+    document.getElementById("jumlahSantri").textContent = dashboard.totalSantri;
+
+    console.log("TahfidzKu berhasil dijalankan 🚀");
+
+});
